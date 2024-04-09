2ac0f98c5ac8f3a3b9c132750ab2698a99ec1737
anurag <anuragdw0710@gmail.com>
2024-04-09 19:21:30 +0530

    Basic setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Container = () => {
+    return <h1>Hello world</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Container />);
